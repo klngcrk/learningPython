19fb6aa1746c8c39a3a8e60b60930ccfceaabd1d
klngcrk <slawcio77@gmail.com>
2017-04-12 19:37:36 +0200

    Added new assigment: undefined_variable

---
+++ undefinedVariable/test_undefined_variable.py
@@ -0,0 +1,25 @@
+import os
+import sys 
+import io
+
+s = sys.stdout
+stream = io.StringIO()
+sys.stdout = stream
+
+try:
+	import undefined_variable
+except NameError:
+	sys.stdout = s
+	print('Kod dalej zwraca ten sam błąd: NameError.')
+	sys.exit(0)
+except Exception:
+	sys.stdout = s
+	print('Dodano nowy błąd do kodu, kod należy poprawiać, a nie psuć bardziej :-)')
+	sys.exit(0)
+
+sys.stdout = s
+received = stream.getvalue()
+
+expected = '4'
+assert  expected in received, "Oczekiwano tekstu zawierajacego\n'{}'\n, a otrzymano:\n'{}'".format(expected,  received if received else 'Pusty tekst')
+print("Success!")
---
+++ undefinedVariable/undefined_variable.py
@@ -0,0 +1,10 @@
+'''W tym programie postaramy się sprawdzić działanie Pythona, gdy popełnimy błąd składniowy
+   Zadanie:
+   1) Uruchom program. Przeanalizuj otrzymany błąd.
+   2) Popraw program tak aby zostala wypisana wartość zmiennej c, która powinna wynosić 4. 
+'''
+a = 2 
+b = 3 
+d = 5
+
+print(c)
