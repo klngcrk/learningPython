25963808d70125760f5bf7e227ab62ade6b6d853
klngcrk <slawcio77@gmail.com>
2017-04-12 18:51:01 +0200

    Initail commit

---
+++ Introduction/hello_world.py
@@ -0,0 +1,6 @@
+'''Tradycyjnie pierwszy program napisany w dowolnym języku, powinien zaczynać się 
+	od przywitania ze światem, czyli 'Hello world!'
+
+	Zadanie:
+	Napisać program wypisujący tekst "Hello world! Nazywam się <tutaj wstaw swoje imie>"
+'''
---
+++ Introduction/test_hello_world.py
@@ -0,0 +1,15 @@
+import os
+import sys 
+import io
+
+s = sys.stdout
+stream = io.StringIO()
+sys.stdout = stream
+
+import hello_world
+
+sys.stdout = s
+received = stream.getvalue()
+
+expected = 'Hello world! Nazywam się'
+assert  expected in received, "Oczekiwano tekstu zawierajacego\n'{}'\n, a otrzymano:\n'{}'".format(expected,  received if received else 'Pusty tekst')
