f6199673e7a670c5db9988e1e4786fe90373bfae
klngcrk <slawcio77@gmail.com>
2017-04-12 18:58:18 +0200

    Corrected hello_word assigment

--- Introduction/hello_world.py
+++ Introduction/hello_world.py
@@ -1,6 +1,8 @@
 '''Tradycyjnie pierwszy program napisany w dowolnym języku, powinien zaczynać się 
-	od przywitania ze światem, czyli 'Hello world!'
+	od przywitania ze światem, czyli 'Hello world!'. Oczywiście przy powitaniu warto się przedstawić.
 
 	Zadanie:
-	Napisać program wypisujący tekst "Hello world! Nazywam się <tutaj wstaw swoje imie>"
+	Napisać program wypisujący tekst: "Hello world! Nazywam się <tutaj wstaw swoje imie>"
+
+	Podpowiedź: użyj funkcji print()
 '''
