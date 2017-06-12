e2c82dac9a9950d2306ae84da0b4d711d3837831
melisssaoff <melisssaoff@o2.pl>
2017-06-12 10:50:02 +0200

    Update userInput.py

--- Strings/userInput.py
+++ Strings/userInput.py
@@ -13,3 +13,20 @@ True
 >>>is_woman_name("Tomek")
 False
 """
+def is_woman_name(imie):
+x=input('wprowadz ime')
+print(x)
+    imie='marta'
+    imie2='piotr'
+    print(len(imie))
+    print(len(imie2))
+    b=(imie[-1])
+    if b=='a':
+        return True
+    else:
+        return False
+    is_woman_name(asia)
+	print(is_woman_name(asia))
+#x=input('wprowadz imie')
+#print(x)
+    
