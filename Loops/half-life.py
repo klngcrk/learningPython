e52df1f7cef5d290bbe0d6bfbdd6e9afd6389303
slawomir.szczurek(GL) <slawomir.szczurek@globallogic.com>
2017-06-22 17:11:25 +0200

    Added loops exercises

---
+++ Loops/half-life.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Write a function that calculates mass of an element after decay for provided time.
+Mass must be calcuted using half-life formula:
+mass = initial_mass * (1/2)^(t/T)
+
+mass 			- output mass [grams]
+initial_mass 	- mass of element at begining [grams]
+t 				- time of decay [years]
+T 				- half-life time [years]
+
+>>> mass_after_decay(10, 15, 5)
+1,25
+"""
+
+
+def mass_after_decay(initial_mass, t, T):
+    pass
---
+++ Loops/iteration.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+
+  (\,/)
+  oo   '''//,        _
+,/_;~,        \,    / '
+"'   \    (    \    !
+      ',|  \    |__.'
+      '~  '~----''
+Write a function that compares masses of the laboratory rats.
+Masess of 2 rats are stored in two lists. Each list relates to each rat.
+Every rat was weighted once a day. Each element in list is result of that measurement.
+Elements in list are sorted chronogically.
+Functiion should print day number when second rat was heavier than first one.
+
+"""
+rat_1_mass = [125, 123, 129, 130, 130, 130, 128, 125, 123, 122]
+rat_2_mass = [125, 126, 126, 123, 124, 126, 128, 130, 130, 130]
+
+
+def mass_compare():
+    pass
