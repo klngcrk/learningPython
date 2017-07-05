d63df407d35a97e5811aa681b8f9f1e2d6261fce
klngcrk <slawcio77@gmail.com>
2017-07-05 21:22:24 +0200

    Basic sets of exercises for sets

---
+++ structs/sets/sets_basic.py
@@ -0,0 +1,21 @@
+"""
+Odpowiedzi na zadania umieszaj bezpośrednio pod poleceniem.
+Zadania z gwiazdką są opcjonalne. Są to rzadko spotykane operacje w codziennej pracy.
+Zadania z podwója gwiazdką są trudne i również są opcjonalne.
+"""
+
+1. Napisz program tworzący nowy set z jednym element oraz drugi pusty set.
+
+2. Napisz program wypisujący wszystkie elementy setu.
+
+3. Napisz program dodający do setu dwa różne stringi.
+
+4. Napisz program usuwający element z setu.
+
+5. Napisz program usuwający element z setu, pod warunkiem, że jest element istnieje w secie.
+
+6. Napisz program wypisujący elementy wspólne dwóch setów.
+
+7*. Napisz program wypisujący różnicę setów (elementy obecne w secie A, których nie ma w secie B)
+
+8*. Napisz program wypisujący wartość maksymalną oraz minimalną z setu.
