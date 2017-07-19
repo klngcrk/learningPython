785131c7e1c947df6bf95830b5ec56c98078fef0
klngcrk <slawcio77@gmail.com>
2017-07-19 20:13:15 +0200

    Added dictionary assigments.

---
+++ Collections/Dictionaries/dicts_basic.py
@@ -0,0 +1,34 @@
+"""
+Odpowiedzi na zadania umieszaj bezpośrednio pod poleceniem.
+Zadania z gwiazdką są opcjonalne. Są to rzadko spotykane operacje w codziennej pracy.
+Zadania z podwója gwiazdką są trudne i również są opcjonalne.
+"""
+
+1. Napisz program dodający do słownika nowy klucz (dowolna warość).
+
+2. Napisz program wypisujący wartość ze słownika dla podanego klucza.
+
+3. Napisz program wypisujący wartość ze słownika dla podanego klucza. Jeżeli klucz nie istnieje w słowniku, program nie zwróci błędu KeyError.
+
+4. Napisz program wypisujący wartość ze słownika dla podanego klucza. Jeżeli klucz nie istnieje w słowniku, program zwróci: None. 
+
+5. Napisz program usuwający klucz ze słownika.
+
+6. Napisz program usuwający klucz ze słownika, pod warunkiem, że klucz w słowniku istnieje. 
+
+7. Napisz program usuwający klucz ze słownika. Jeżeli klucz nie istnieje, program zwróci stringa 'Klucz nie istnieje w tym słowniku'.
+
+8. Napisz program usuwający wszystkie klucze ze słownika.
+
+9. Napisz program wypisujący wszyskie klucze ze słownika.
+
+10. Napisz program wypisujący wszystkie wartości ze słownika.
+
+11. Napisz program dodający do istniejącego słownika, drugi słownik.
+
+12. Napisz program wypisujący minimalną oraz maksymalną wartość ze słownika.
+
+13. Napisz program przechodzący zwracający iloczyn wartości słownika. 
+
+13.** Napisz program tworzący słownik z dwóch list, pierwsza lista reprezentuje klucze, druga wartości. Należy zachować kolejność list,
+	 pierwszy klucz odpowiada pierwszej wartości itd. 
