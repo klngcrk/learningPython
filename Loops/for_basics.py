aee450e1d5bf2314f60ccc0666071f1e1af3bf43
slawomir.szczurek(GL) <slawomir.szczurek@globallogic.com>
2017-08-01 17:33:51 +0200

    Added 'loops' exercises

---
+++ Loops/for_basics.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Odpowiedzi na zadania umieszaj bezpośrednio pod poleceniem.
+Zadania z gwiazdką są trudniejsze i są opcjonalne.
+Zadania z podwója gwiazdką są bardzo trudne i również są opcjonalne.
+"""
+
+
+# region zadania z funkcją range()
+# 1. Napisz program wypisujący co drugą liczbę z przedziału <1, 50>
+
+# 2. Napisz program wypisujący kwadraty liczb z przedziału <1, 10>
+
+# 3. Napisz program wypisujący liczby z przedziału <100, 1>
+
+# 4. Napisz program wypisujący liczby podzielne przez 8.
+
+# 5. Napisz program wypisujący liczby podzielne przez 3 lub 5.
+
+# 6. Napisz program wypisujący liczby podzielne przez 2 i 3.
+
+# 7*. | Napisz program znajdujący największą liczbę podzielną przez 2,3,5,7
+#     | ale mniejszą od 1000
+
+# 8* | Napisz program wypisujący pojedynczo(!) każdą liczbę z poniższej listy:
+lista = [[1, 2, 3], [9, 10, 11], [20, 30, 40]]
+
+# endregion
+# region zadania z iterowaniem kolekcji
+
+# 9. Napisz program wypisujący litery ze stringa.
+
+# 10. Napisz program wypisujący kolejne słowa ze stringa.
+
+# 11. Napisz program wypisujący liczbę cyfr z liczby.
+
+# 12. Napisz program wypisujący cyfry z liczby. Cyfry nie mogą się powtarzać.
+
+# 13. Napisz program sprawdzający czy wszystkie cyfry liczby są parzyste.
+
+# 14. | Napisz program wypisujący zadaną liczbę zastępując jej kolejne cyfry liczbami powstałymi przez
+#     | dodanie do cyfr liczby 1 (1598 = 26109)
+
+# 15. | Napisz program wypisujący przywitania z Pythonem w podanych poniżej językach świata.
+#     | Oczekiwany wynik: 'Hello Python' i tak dalej dla każdego języka.
+hellos = [
+    "Hello",
+    "Tungjatjeta",
+    "Grüßgott",
+    "Вiтаю",
+    "dobrý den",
+    "hyvää päivää",
+    "你好",
+    "早上好"]
+
+# 16**. | Napisz program sprawdzający czy numer PESEL jest poprawny.
+#      | opis: https://pl.wikipedia.org/wiki/PESEL#Cyfra_kontrolna_i_sprawdzanie_poprawno.C5.9Bci_numeru
---
+++ Loops/rysunki.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+ Zadanie są o podwyższonym poziomie trudności.
+
+ Zadanie należy wykonąć z użyciem pętli for.
+ Rozmiary figur powinny być parmatryzowane, przykład.:
+    Wielkość trójkąta w zadaniu 2, powinna być możliwa do zmienienia,
+    i tak trójkąt o wielkości 3, powinien wyglądać tak:
+    O
+    OO
+    OOO
+'''
+
+# 1. Napisz program wypisujący 17 gwiazdek (znak '*')
+
+# 2. | Napisz program wypisujący wieżę z liter 'O':
+#    | Oczekiwany wynik:
+'''
+O
+OO
+OOO
+OOOO
+OOOOO
+'''
+
+# 3. Napisz program wypisujący prostokąt z liter 'X'. Oczekiwany wynik:
+'''
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+XXXXXXXXXX
+'''
+
+# 4. Napisz program wypisujący trójkąt z liter 'X'. Oczekiwany wynik:
+'''
+    X
+   XXX
+  XXXXX
+ XXXXXXX
+XXXXXXXXX
+'''
+
+# 5. Napisz program wypisujący kwadrat z liter 'K': Oczekiwany wynik:
+'''
+KKKK
+KKKK
+KKKK
+KKKK
+'''
