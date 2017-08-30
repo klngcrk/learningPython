04fea8f1c8fdec84f2ce42e3175a8cf863bd39de
klngcrk <slawcio77@gmail.com>
2017-08-30 18:25:07 +0200

    Added new exercies 'menu' to Loops

---
+++ Loops/menu.py
@@ -0,0 +1,17 @@
+"""
+Zadaniem jest napisanie programu, który będzie tekstowym menu. Program po uruchomieniu będzie wypisał menu z opcjami.
+Następnie poprosi użytkonika o wybranie opcji. Wybranie opcji wypisze użytkownikowi, którą pozycję z menu wybrał 
+(np. Wybrałeś opcję numer 3). Nastepnie użytkownikowi zostanie wypisane ponownie to samo menu (tak w kółko, 
+aż nie wybierze ostatniej opcji). Ostatnia opcja z menu musi pozwolić użytkonikowi na zakończenie działania programu. 
+Szablon menu:
+
+***Menu***
+1. Wciśnij klawisz '1', aby wybrać pierwszą opcję
+2. Wciśnij klawisz '2', aby wybrać drugą opcję
+3. Wciśnij klawisz '3', aby wybrać trzecią opcję
+
+0. Wciśnij klawisz '0', aby zakończyć.
+**********
+
+Podpowiedź, użyj funkcji input(), aby pobrać dane od użytkownika.
+"""
